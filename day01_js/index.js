4c91ebe7be619dab7755fe246dab47d79e540927
BalesabuGodugu <godugubalesabu@gmail.com>
2019-06-04 21:57:25 +0530

    added examples

---
+++ day01_js/index.js
@@ -0,0 +1,33 @@
+//console.log('welcome to javacript course')
+
+// this is a single line comment
+
+/*
+   this is a course on javascript 
+   we will cover very indepth of javascript topics.
+   it is fully hands on course.
+
+
+*/
+/*
+var courseduration = 30 // number data type
+
+var courseduration2 = '30' // string data type
+
+
+ 
+if (courseduration === courseduration2) {
+  console.log('true')
+} else {
+  console.log('false')
+}
+*/
+
+var coursename = 'javascript course'
+
+console.log(coursename)
+console.log(coursename.length)
+console.log(coursename.includes('u'))
+console.log(coursename.toUpperCase())
+console.log(coursename.replace('javascript', 'advanced js'))
+console.log(coursename)
