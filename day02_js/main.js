57ea515a6f2aa843f1d28e0ebbaae01d1f28397e
BalesabuGodugu <godugubalesabu@gmail.com>
2019-06-06 22:15:21 +0530

    updated day02

---
+++ day02_js/main.js
@@ -0,0 +1,37 @@
+/*console.log(coursetopic)
+var coursetopic = 'angular'
+console.log(coursetrainer)
+let coursetrainer = 'react'
+*/
+// function definition
+function getMyName() {
+  console.log('trainer')
+}
+// invoking a funciton
+//getMyName()
+//
+
+function addValues(a, b) {
+  return a + b
+}
+
+//console.log(addValues(2, 3))
+
+// arrow function
+
+//addValues = () => console.log('test')
+
+//addValues()
+ananimous function
+/*
+;(() => {
+  console.log('new test function')
+})()
+*/
+
+
+function A(){
+  function B(){
+    
+  }
+}
